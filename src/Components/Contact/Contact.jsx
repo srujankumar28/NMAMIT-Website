21fd7cabcc7699394ccb7edf80a1718bb8132135
Srujan Kumar <raosrujan28@gmail.com>
2024-03-15 21:30:32 +0530

    commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
         <h3>Send us a message <img src={msg_icon} alt="" /></h3>
         <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
         <ul>
-            <li><img src={mail_icon} alt="" />Contact@NMAMIT.nitte</li>
+            <li><img src={mail_icon} alt="" />info@nitte.edu.in</li>
             <li><img src={phone_icon} alt="" />+91 8147107329</li>
             <li><img src={location_icon} alt="" />NMAMIT-Nitte<br/> Mangalore, Karnataka</li>
         </ul>
